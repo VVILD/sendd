2e1bc381878353e8ed0dc97fb2457f41aef096fc
sargun <sargungu@gmail.com>
2015-06-03 20:59:16 +0530

    edit/del product

--- businessapp/api.py
+++ businessapp/api.py
@@ -23,11 +23,11 @@ Add CORS headers for tastypie APIs
 
 Usage:
    class MyModelResource(CORSModelResource):
-       ...
-       
+	   ...
+	   
    class MyResource(CORSResource):
-       ...
-       
+	   ...
+	   
 Authors:
    original source by http://codeispoetry.me/index.php/make-your-django-tastypie-api-cross-domain/
    extensions by @miraculixx
@@ -44,98 +44,98 @@ from tastypie.resources import Resource, ModelResource
 import logging
 
 class BaseCorsResource(Resource):
-    """
-    Class implementing CORS
-    """
-    def error_response(self, *args, **kwargs):
-        response = super(BaseCorsResource, self).error_response(*args, **kwargs)
-        return self.add_cors_headers(response, expose_headers=True)
-        
-    def add_cors_headers(self, response, expose_headers=False):
-        response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Headers'] = 'content-type, authorization, x-requested-with, x-csrftoken'
-        if expose_headers:
-            response['Access-Control-Expose-Headers'] = 'Location'
-        return response    
-    
-    def create_response(self, *args, **kwargs):
-        """
-        Create the response for a resource. Note this will only
-        be called on a GET, POST, PUT request if 
-        always_return_data is True
-        """
-        response = super(BaseCorsResource, self).create_response(*args, **kwargs)
-        return self.add_cors_headers(response)
+	"""
+	Class implementing CORS
+	"""
+	def error_response(self, *args, **kwargs):
+		response = super(BaseCorsResource, self).error_response(*args, **kwargs)
+		return self.add_cors_headers(response, expose_headers=True)
+		
+	def add_cors_headers(self, response, expose_headers=False):
+		response['Access-Control-Allow-Origin'] = '*'
+		response['Access-Control-Allow-Headers'] = 'content-type, authorization, x-requested-with, x-csrftoken'
+		if expose_headers:
+			response['Access-Control-Expose-Headers'] = 'Location'
+		return response    
+	
+	def create_response(self, *args, **kwargs):
+		"""
+		Create the response for a resource. Note this will only
+		be called on a GET, POST, PUT request if 
+		always_return_data is True
+		"""
+		response = super(BaseCorsResource, self).create_response(*args, **kwargs)
+		return self.add_cors_headers(response)
 
-    def post_list(self, request, **kwargs):
-        """
-        In case of POST make sure we return the Access-Control-Allow Origin
-        regardless of returning data
-        """
-        #logger.debug("post list %s\n%s" % (request, kwargs));
-        response = super(BaseCorsResource, self).post_list(request, **kwargs)
-        return self.add_cors_headers(response, True)
-    
-    def post_detail(self, request, **kwargs):
-        """
-        In case of POST make sure we return the Access-Control-Allow Origin
-        regardless of returning data
-        """
-        #logger.debug("post detail %s\n%s" (request, **kwargs));
-        response = super(BaseCorsResource, self).post_list(request, **kwargs)
-        return self.add_cors_headers(response, True)
-    
-    def put_list(self, request, **kwargs):
-        """
-        In case of PUT make sure we return the Access-Control-Allow Origin
-        regardless of returning data
-        """
-        response = super(BaseCorsResource, self).put_list(request, **kwargs)
-        return self.add_cors_headers(response, True)    
-    
-    def put_detail(self, request, **kwargs):
-        response = super(BaseCorsResource, self).put_detail(request, **kwargs)
-        return self.add_cors_headers(response, True)
-        
-    def method_check(self, request, allowed=None):
-        """
-        Check for an OPTIONS request. If so return the Allow- headers
-        """
-        if allowed is None:
-            allowed = []
-            
-        request_method = request.method.lower()
-        allows = ','.join(map(lambda s: s.upper(), allowed))
+	def post_list(self, request, **kwargs):
+		"""
+		In case of POST make sure we return the Access-Control-Allow Origin
+		regardless of returning data
+		"""
+		#logger.debug("post list %s\n%s" % (request, kwargs));
+		response = super(BaseCorsResource, self).post_list(request, **kwargs)
+		return self.add_cors_headers(response, True)
+	
+	def post_detail(self, request, **kwargs):
+		"""
+		In case of POST make sure we return the Access-Control-Allow Origin
+		regardless of returning data
+		"""
+		#logger.debug("post detail %s\n%s" (request, **kwargs));
+		response = super(BaseCorsResource, self).post_list(request, **kwargs)
+		return self.add_cors_headers(response, True)
+	
+	def put_list(self, request, **kwargs):
+		"""
+		In case of PUT make sure we return the Access-Control-Allow Origin
+		regardless of returning data
+		"""
+		response = super(BaseCorsResource, self).put_list(request, **kwargs)
+		return self.add_cors_headers(response, True)    
+	
+	def put_detail(self, request, **kwargs):
+		response = super(BaseCorsResource, self).put_detail(request, **kwargs)
+		return self.add_cors_headers(response, True)
+		
+	def method_check(self, request, allowed=None):
+		"""
+		Check for an OPTIONS request. If so return the Allow- headers
+		"""
+		if allowed is None:
+			allowed = []
+			
+		request_method = request.method.lower()
+		allows = ','.join(map(lambda s: s.upper(), allowed))
 
-        if request_method == 'options':
-            response = HttpResponse(allows)
-            response['Access-Control-Allow-Origin'] = '*'
-            response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With, X-CSRFToken, X-HTTP-Method-Override'
-            response['Access-Control-Allow-Methods'] = "GET, PUT, POST, PATCH"
-            response['Allow'] = allows
-            raise ImmediateHttpResponse(response=response)
+		if request_method == 'options':
+			response = HttpResponse(allows)
+			response['Access-Control-Allow-Origin'] = '*'
+			response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With, X-CSRFToken, X-HTTP-Method-Override'
+			response['Access-Control-Allow-Methods'] = "GET, PUT, POST, PATCH"
+			response['Allow'] = allows
+			raise ImmediateHttpResponse(response=response)
 
-        if not request_method in allowed:
-            response = http.HttpMethodNotAllowed(allows)
-            response['Allow'] = allows
-            raise ImmediateHttpResponse(response=response)
+		if not request_method in allowed:
+			response = http.HttpMethodNotAllowed(allows)
+			response['Allow'] = allows
+			raise ImmediateHttpResponse(response=response)
 
-        return request_method
-    
-    def wrap_view(self, view):
-        @csrf_exempt
-        def wrapper(request, *args, **kwargs):
-            request.format = kwargs.pop('format', None)
-            wrapped_view = super(BaseCorsResource, self).wrap_view(view)
-            return wrapped_view(request, *args, **kwargs)
-        return wrapper
+		return request_method
+	
+	def wrap_view(self, view):
+		@csrf_exempt
+		def wrapper(request, *args, **kwargs):
+			request.format = kwargs.pop('format', None)
+			wrapped_view = super(BaseCorsResource, self).wrap_view(view)
+			return wrapped_view(request, *args, **kwargs)
+		return wrapper
 
 #Base Extended Abstract Model
 class CORSModelResource(BaseCorsResource, ModelResource):
-    pass
+	pass
 
 class CORSResource(BaseCorsResource, Resource):
-    pass
+	pass
 
 class MultipartResource(object):
 	def deserialize(self, request, data, format=None):
@@ -258,48 +258,64 @@ class ProductResource(CORSModelResource):
 		return base_object_list
 	
 	def hydrate(self,bundle):
-		print "hhhhhhhhhhhh"
 		try:
-			business=Business.objects.get(pk=bundle.data['username'])
-			bundle.data['business']="/bapi/v1/business/"+str(bundle.data['username'])+"/"
-			print "user identified"
+			override_method=bundle.request.META['HTTP_X_HTTP_METHOD_OVERRIDE']
+			print "changed to PATCH"
 		except:
-			#print "fuck"
-			bundle.data['business']="/bapi/v1/business/0/"
-			bundle.data["msg"]='notregistered'
-			print "user not identified"
+			override_method='none'
+			print "hello"
+
+		if bundle.request.META['REQUEST_METHOD'] == 'POST' and override_method!='PATCH':
+			print "hhhhhhhhhhhh"
+			try:
+				business=Business.objects.get(pk=bundle.data['username'])
+				bundle.data['business']="/bapi/v1/business/"+str(bundle.data['username'])+"/"
+				print "user identified"
+			except:
+				#print "fuck"
+				bundle.data['business']="/bapi/v1/business/0/"
+				bundle.data["msg"]='notregistered'
+				print "user not identified"
+				return bundle
+
+			#create order
+	#curl --dump-header - -H "Content-Type: application/json" -X POST --data '{ "username": "newuser3", "name": "asd" , "phone":"8879006197","street_address":"office no 307, powai plaza","city":"mumbai","state":"maharashtra" ,"pincode":"400076","country":"india" , "payment_method":"F" ,"pname":"['clothes','books']","pprice":"['50','60']" ,"pweight":"['2','7']" }' http://127.0.0.1:8000/bapi/v1/order/		
+			try:
+				order =Order.objects.create(business=business,name=bundle.data['name'],phone=bundle.data['phone'],street_address=bundle.data['street_address'],city=bundle.data['city'],state=bundle.data['state'],pincode=bundle.data['pincode'],country=bundle.data['country'],payment_method=bundle.data['payment_method'])
+				print "order created	"
+
+				try:
+					print "check here"
+					#print bundle.data['array'][0]
+					#print len(bundle.data['array'])
+
+					for x in range (0,len(bundle.data['pname'])-1):
+						product =Product.objects.create(order=order,name=bundle.data['pname'][x],weight=bundle.data['pweight'][x],price=bundle.data['pprice'][x])					
+
+				except:
+					bundle.data['errormsg']='error creating product'
+			
+			except:
+				bundle.data['errormsg']='error creating order'
+
+			x=x+1
+			bundle.data['name']=str(bundle.data['pname'][x])
+			bundle.data['weight']=str(bundle.data['pweight'][x])
+			bundle.data['price']=str(bundle.data['pprice'][x])
+
+			bundle.data['order']="/bapi/v1/order/"+ str(order.pk) + "/"
+
 			return bundle
 
-		#create order
-#curl --dump-header - -H "Content-Type: application/json" -X POST --data '{ "username": "newuser3", "name": "asd" , "phone":"8879006197","street_address":"office no 307, powai plaza","city":"mumbai","state":"maharashtra" ,"pincode":"400076","country":"india" , "payment_method":"F" ,"pname":"['clothes','books']","pprice":"['50','60']" ,"pweight":"['2','7']" }' http://127.0.0.1:8000/bapi/v1/order/		
-		try:
-			order =Order.objects.create(business=business,name=bundle.data['name'],phone=bundle.data['phone'],street_address=bundle.data['street_address'],city=bundle.data['city'],state=bundle.data['state'],pincode=bundle.data['pincode'],country=bundle.data['country'],payment_method=bundle.data['payment_method'])
-			print "order created	"
 
-			try:
-				print "check here"
-				#print bundle.data['array'][0]
-				#print len(bundle.data['array'])
 
-				for x in range (0,len(bundle.data['pname'])-1):
-					product =Product.objects.create(order=order,name=bundle.data['pname'][x],weight=bundle.data['pweight'][x],price=bundle.data['pprice'][x])					
-
-			except:
-				bundle.data['errormsg']='error creating product'
-		
-		except:
-			bundle.data['errormsg']='error creating order'
-
-		x=x+1
-		bundle.data['name']=str(bundle.data['pname'][x])
-		bundle.data['weight']=str(bundle.data['pweight'][x])
-		bundle.data['price']=str(bundle.data['pprice'][x])
-
-		bundle.data['order']="/bapi/v1/order/"+ str(order.pk) + "/"
-
-		return bundle
+		if bundle.request.META['REQUEST_METHOD'] == 'POST' and override_method=='PATCH':
+			print "patch"
+			return bundle
 
 	def dehydrate(self,bundle):
+
+		
 		try:
 			pk=bundle.data['order'].split('/')[4]
 			order=Order.objects.get(pk=pk)
@@ -311,7 +327,6 @@ class ProductResource(CORSModelResource):
 			
 			bundle.data['order_no']=436+int(bundle.data['id'])
 			print bundle.data['order_no']
-
 		except:
 			print "shit"
 		return bundle
@@ -643,15 +658,15 @@ class ShipmentResource2(MultipartResource,ModelResource):
 
 	def prepend_urls(self):
 		return [
-            url(r"^(?P<resource_name>%s)/(?P<real_tracking_no>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
-        ]
+			url(r"^(?P<resource_name>%s)/(?P<real_tracking_no>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+		]
 
 
 
 	#def over_urls(self):
 	#	return [
-    #   	url(r'^(?P<resource_name>%s)/(?P<track>[\w\.-]+)/$' % self._meta.resource_name, self.wrap_view('dispatch_detail'), name='api_dispatch_detail_track'),
-    #        ]
+	#   	url(r'^(?P<resource_name>%s)/(?P<track>[\w\.-]+)/$' % self._meta.resource_name, self.wrap_view('dispatch_detail'), name='api_dispatch_detail_track'),
+	#        ]
 
 	def build_filters(self, filters=None):
 		print "shit"
