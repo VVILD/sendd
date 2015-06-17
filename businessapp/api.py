39c08cc9c57739d6fd9e93b08b5785264cefa4eb
sargun <sargungu@gmail.com>
2015-06-17 21:45:22 +0530

    tracking business

--- businessapp/api.py
+++ businessapp/api.py
@@ -422,7 +422,7 @@ class OrderResource(CORSModelResource):
 				print "asd"
 				raw_data=' '
 				for x in range (0,len(tracking_json)):
-					raw_data=raw_data+ tracking_json[-1]['status'].encode('ascii','ignore') +"&nbsp; &nbsp;"+ tracking_json[-1]['date'].encode('ascii','ignore')+"&nbsp; &nbsp;"+ tracking_json[-1]['location'].encode('ascii','ignore')+"<br>"
+					raw_data=raw_data+ tracking_json[x]['status'].encode('ascii','ignore') +"&nbsp; &nbsp;"+ tracking_json[x]['date'].encode('ascii','ignore')+"&nbsp; &nbsp;"+ tracking_json[x]['location'].encode('ascii','ignore')+"<br>"
 				
 				print raw_data
 				print '3.5'
