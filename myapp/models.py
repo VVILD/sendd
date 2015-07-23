77ac0821bc2db84d5eb4f63aba775267783be4c6
Vatsal Shah <shahvatsal90@gmail.com>
2015-07-23 13:21:20 +0530

    Trying a fix for migration

--- myapp/models.py
+++ myapp/models.py
@@ -7,8 +7,6 @@ from django.core.validators import RegexValidator
 from pytz import timezone
 from push_notifications.models import GCMDevice
 
-from pickupboyapp.models import PBUser
-
 
 class Pricing(models.Model):
     amount_charged_by_courier = models.IntegerField(null=True, blank=True)
@@ -122,7 +120,7 @@ class Order(models.Model):
     #cancelled=models.CharField(max_length=1,
     # choices=(('Y','yes') ,('N','no'),),
     #								  default='N')
-    pb = models.ForeignKey(PBUser, null=True, blank=True)
+    pb = models.ForeignKey('pickupboyapp.PBUser', null=True, blank=True)
     latitude = models.DecimalField(max_digits=25, decimal_places=20, null=True, blank=True)
     longitude = models.DecimalField(max_digits=25, decimal_places=20, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
