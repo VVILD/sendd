dd2526323e75a68c2a9494e3d1e526bcc55d532a
Vatsal Shah <shahvatsal90@gmail.com>
2015-07-28 15:08:33 +0530

    Fixed more bugs

--- pickupboyapp/admin.py
+++ pickupboyapp/admin.py
@@ -62,7 +62,7 @@ class PickupboyAdmin(admin.ModelAdmin):
         pb_users = PBUser.objects.all()
         for pb_user in pb_users:
             pb_location = PBLocations.objects.filter(pbuser=pb_user).order_by('-updated_at')
-            if pb_location is not None:
+            if len(pb_location) > 0:
                 location_map.append([pb_user.name, pb_location[0].lat, pb_location[0].lon])
         location_map_json = json.dumps(location_map)
         return super(PickupboyAdmin, self).changelist_view(request, extra_context={"location_map": location_map_json})
