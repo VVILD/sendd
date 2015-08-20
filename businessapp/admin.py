d63c3ad997cb670515abee06cbc997b17e96b933
sargun <sargungu@gmail.com>
2015-08-20 18:45:08 +0530

    city/state search

--- businessapp/admin.py
+++ businessapp/admin.py
@@ -380,7 +380,7 @@ class FilterUserAdmin(admin.ModelAdmin):
 
 class OrderAdmin(FilterUserAdmin):
     inlines = (ProductInline,)
-    search_fields = ['business__business_name', 'name', 'product__real_tracking_no', 'product__barcode']
+    search_fields = ['business__business_name', 'name', 'product__real_tracking_no', 'product__barcode','city','state']
     list_display = (
         'order_no', 'book_time', 'business_details', 'name', 'status', 'no_of_products', 'total_shipping_cost',
         'total_cod_cost', 'method')
