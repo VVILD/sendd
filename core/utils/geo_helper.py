067ea8ccb4cdff8362f6f46103e7d1e6a1009b56
Vatsal Shah <shahvatsal90@gmail.com>
2015-08-31 16:07:41 +0530

    Not finished yet

--- core/utils/geo_helper.py
+++ core/utils/geo_helper.py
@@ -1,13 +1,15 @@
 __author__ = 'vatsalshah'
 
-from geopy.geocoders import googlev3
+from geopy.geocoders import GeoNames, googlev3
 from geopy.distance import vincenty
 
 geolocator = googlev3.GoogleV3(api_key="AIzaSyBEfEgATQeVkoKUnaB4O9rIdX2K2Bsh63o")
-location = geolocator.geocode("Classique Centre, Gundavali, Andheri East, Mumbai")
+# geolocator = GeoNames()
+location = geolocator.geocode("400076, India")
 print(location.address)
 location_lat_long = (location.latitude, location.longitude)
-location2 = geolocator.geocode("BOX8 Marol Naka, Marol Maroshi Road, Andheri East, Mumbai")
-print(location2.address)
-location2_lat_long = (location2.latitude, location2.longitude)
-print(vincenty(location_lat_long, location2_lat_long).kilometers)
+print(location_lat_long)
+# location2 = geolocator.geocode("BOX8 Marol Naka, Marol Maroshi Road, Andheri East, Mumbai")
+# print(location2.address)
+# location2_lat_long = (location2.latitude, location2.longitude)
+# print(vincenty(location_lat_long, location2_lat_long).kilometers)
