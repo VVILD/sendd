247165bcb211b64402444bd5a29dea1830b96070
Vatsal Shah <shahvatsal90@gmail.com>
2015-09-12 12:31:14 +0530

    Added pincode admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import Warehouse
+from core.models import Warehouse, Pincode
 
 __author__ = 'vatsalshah'
 
@@ -9,3 +9,11 @@ class WarehouseAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Warehouse, WarehouseAdmin)
+
+
+class PincodeAdmin(admin.ModelAdmin):
+    list_display = ('pincode', 'division_name', 'region_name', 'circle_name', 'taluk', 'district_name', 'state_name', 'fedex_oda_opa', 'fedex_cod_service', 'fedex_servicable')
+    search_fields = ('pincode', 'division_name', 'region_name', 'circle_name', 'taluk', 'district_name', 'state_name')
+    list_filter = ('region_name', 'district_name', 'state_name', 'fedex_cod_service', 'fedex_oda_opa', 'fedex_servicable')
+
+admin.site.register(Pincode, PincodeAdmin)
