be05ca546e0bd4340ed362f3c2774c5456ba1e27
sargun <sargungu@gmail.com>
2015-09-19 12:48:42 +0530

    unicode bugfix

--- myapp/models.py
+++ myapp/models.py
@@ -55,7 +55,7 @@ class Address(models.Model):
     country = models.CharField(max_length=30, null=True, blank=True)
 
     def __unicode__(self):
-        return 'pk:'str(self.pk)
+        return 'pk:'+str(self.pk)
 
     def save(self, *args, **kwargs):
         if not state_matcher.is_state(self.state):
