63da55bd8892f62cb298f0f82bc446799a6a5955
sargun <sargungu@gmail.com>
2015-09-23 12:42:27 +0530

    sms bugfix

--- core/management/commands/sms.py
+++ core/management/commands/sms.py
@@ -33,15 +33,18 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
   	x=Business.objects.filter()
 
-	for y in x:
-	    address=urllib.quote_plus(str(y.address))  
-	    phone=urllib.quote_plus(str(8879006197))
-	    user_phone=urllib.quote_plus(str(y.contact_office)+str(y.contact_mob))
-	    order_no=urllib.quote_plus(str(y.pk))
-	    name=urllib.quote_plus(str(y.name))
-	    msg0 = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="
-	    msga = str(phone)
-	    msg1 = "&msg=Pickup+details+for+order+no%3A"+str(order_no)+".%0D%0AName%3A"+str(name)+"%2C+Address%3A"+str(address)+"%2C+Mobile+No%3A"+str(user_phone)+"&msg_type=TEXT&userid=2000142364&auth_scheme=plain&password=h0s6jgB4N&format=text"
-	    query = ''.join([msg0, msga, msg1])
-	    req = requests.get(query)
-	    print y.username+",",req
+  	try:
+		for y in x:
+		    address=urllib.quote_plus(str(y.address))  
+		    phone=urllib.quote_plus(str(8879006197))
+		    user_phone=urllib.quote_plus(str(y.contact_office)+str(y.contact_mob))
+		    order_no=urllib.quote_plus(str(y.pk))
+		    name=urllib.quote_plus(str(y.name))
+		    msg0 = "http://enterprise.smsgupshup.com/GatewayAPI/rest?method=SendMessage&send_to="
+		    msga = str(phone)
+		    msg1 = "&msg=Pickup+details+for+order+no%3A"+str(order_no)+".%0D%0AName%3A"+str(name)+"%2C+Address%3A"+str(address)+"%2C+Mobile+No%3A"+str(user_phone)+"&msg_type=TEXT&userid=2000142364&auth_scheme=plain&password=h0s6jgB4N&format=text"
+		    query = ''.join([msg0, msga, msg1])
+		    req = requests.get(query)
+		    print y.username+",",req
+	except:
+		print "failed",y.username
