7f190b2540fa7838a6c6fbfc2a511f7896c4c93b
sargun <sargungu@gmail.com>
2015-11-09 22:53:08 +0530

    return in form

--- businessapp/admin.py
+++ businessapp/admin.py
@@ -1972,7 +1972,7 @@ class QcProductAdmin(reversion.VersionAdmin,ImportExportActionModelAdmin):
 			self.form = NewTrackingStatus
 			self.fieldsets = (
 				('Add new',
-				 {'fields': [('nstatus', 'ttime', 'location'), ], 'classes': ('suit-tab', 'suit-tab-general')}),
+				 {'fields': ['nstatus', 'ttime', 'location', ], 'classes': ('suit-tab', 'suit-tab-general')}),
 				('Previous tracking', {'fields': ['p_tracking', ],
 									   'classes': ('suit-tab', 'suit-tab-general')}),
 				('Do not edit this', {'fields': ['tracking_data','status' ], 'classes': ('suit-tab', 'suit-tab-tracking')}),
