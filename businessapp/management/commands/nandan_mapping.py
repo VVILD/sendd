c583a2ce458763cd298e869464d40172946d85db
Vatsal Shah <shahvatsal90@gmail.com>
2015-11-17 23:52:52 +0530

    For a different format

--- businessapp/management/commands/nandan_mapping.py
+++ businessapp/management/commands/nandan_mapping.py
@@ -1,6 +1,8 @@
 import csv
 from optparse import make_option
 import os
+
+from datetime import datetime
 from django.core.management import BaseCommand, CommandError
 
 from businessapp.models import Order
@@ -35,11 +37,20 @@ class Command(BaseCommand):
                 try:
                     order = Order.objects.get(order_no=row["order_no"])
                     for product in order.product_set.all():
-                        product.mapped_tracking_no = row["AWB NO"]
-                        product.company = 'NA'
+                        if "AWB NO" in row:
+                            product.mapped_tracking_no = row["AWB NO"]
+                            product.company = 'NA'
                         if 'STATUS' in row:
                             if row['STATUS'] == 'DELIVERED':
                                 product.status = 'C'
+                                if 'DELIVERY DATE' in row:
+                                    try:
+                                        delivery_time = datetime.strptime(row['DELIVERY DATE'], "%d/%m/%y")
+                                        product.actual_delivery_timestamp = delivery_time
+                                    except:
+                                        pass
+                            elif row['STATUS'] == 'Return':
+                                product.status = 'R'
                         product.save()
                     mapped_orders.append(order)
                     print("awb {} mapped for order no {} of business {}".format(row["AWB NO"], order.order_no, order.business.business_name))
