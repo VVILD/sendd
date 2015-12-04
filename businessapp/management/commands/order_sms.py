636196fae228876092fd6f70ff1ebe8c488bd9d1
sargun <sargungu@gmail.com>
2015-12-04 15:55:26 +0530

    updating prices

--- businessapp/management/commands/order_sms.py
+++ businessapp/management/commands/order_sms.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
 from businessapp.models import Product, Order
 from myapp.models import Shipment
-import easypost
-import aftership
-api = aftership.APIv4('c46ba2ea-5a3e-43c9-bda6-793365ec1ebb')
+#import easypost
+#import aftership
+#api = aftership.APIv4('c46ba2ea-5a3e-43c9-bda6-793365ec1ebb')
 import json
 import time
 import dateutil.parser
